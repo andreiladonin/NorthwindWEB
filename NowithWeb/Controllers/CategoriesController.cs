42a66ed5bf95f94d50b95e7f589afdd09ce0ffdb
user-bot001 <dron201202@gmail.com>
2021-09-26 20:42:53 +0400

    Добавьте файлы проекта.

---
+++ NowithWeb/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NowithWeb.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly NorthwindContext _db;
+
+        public CategoriesController(NorthwindContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> obj = _db.Categories.ToList();
+            return View(obj);
+        }
+    }
+}
